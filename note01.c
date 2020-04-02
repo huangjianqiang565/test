fc68eef01d80310292b3bc913748bba15fee787c
jianqiang.huang <1242871950@qq.com>
2020-04-01 23:38:42 -0700

    add note01.c note01.h

---
+++ note01.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void note01(void)
+{
+	printf("FILE:		%s\nFUNC:		%s\nLINE:		%d\n" , __FILE__,__FUNCTION__,__LINE__);
+}
---
+++ note01.h
@@ -0,0 +1,6 @@
+#ifndef NOTE01_H
+#define NOTE01_H
+
+void note01(void);
+
+#endif
