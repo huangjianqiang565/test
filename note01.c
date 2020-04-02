b923ecb109956dbd9a77ae3261ff66bd8610d315
jianqiang.huang <1242871950@qq.com>
2020-04-01 23:46:59 -0700

    add note01.c note01.h

--- note01.c
+++ note01.c
@@ -2,5 +2,5 @@
 
 void note01(void)
 {
-	printf("FILE:		%s\nFUNC:		%s\nLINE:		%d\n" , __FILE__,__FUNCTION__,__LINE__);
+	printf("FILE:	%s\nFUNC:	%s\nLINE:	%d\n" , __FILE__,__FUNCTION__,__LINE__);
 }
